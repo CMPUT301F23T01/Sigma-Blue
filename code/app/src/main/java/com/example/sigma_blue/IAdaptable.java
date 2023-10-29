baf976e98f1c198b259942e2c7307e664c91749d
Bach <waridh@ualberta.ca>
2023-10-29 12:15:35 -0600

    created the skeleton for the ItemListAdapter.
    Stopping work on this branch until the other components have more progress.

---
+++ code/app/src/main/java/com/example/sigma_blue/IAdaptable.java
@@ -0,0 +1,11 @@
+package com.example.sigma_blue;
+
+/**
+ * Interface that enforces methods that are required for a container to be
+ * adaptable to the android BaseAdapter
+ */
+public interface IAdaptable {
+    public int getCount();
+    public int getItem(int position);
+    public int getItemId(int position);
+}
