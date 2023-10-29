278629537e243df1d5f0adc09608a82ec89de118
Bach <waridh@ualberta.ca>
2023-10-29 16:18:02 -0600

    Made the ItemListAdapter work with recylerView

--- code/app/src/main/java/com/example/sigma_blue/ItemListAdapter.java
+++ code/app/src/main/java/com/example/sigma_blue/ItemListAdapter.java
@@ -5,19 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ItemListAdapter extends BaseAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemListAdapter extends
+        RecyclerView.Adapter<ItemListAdapter.ViewHolder> {
+    /* Caching the views in the adapter. */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView make;
+        TextView id;
+
+        /* Constructor that accepts the entire row */
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.itemName);
+            make = itemView.findViewById(R.id.itemMake);
+            id = itemView.findViewById(R.id.uniqueId);
+        }
+    }
+
     /* Attributes */
     private ItemList itemList;
-    private Context applicationContext;
 
     /* Factories and Constructors */
-
-    public static ItemListAdapter newInstance(ItemList itemList, Context context) {
-        ItemListAdapter ret = new ItemListAdapter(itemList);
-        ret.setContext(context);
-        return ret;
-    }
 
     public static ItemListAdapter newInstance(ItemList itemList) {
         return new ItemListAdapter(itemList);
@@ -36,7 +52,6 @@ public class ItemListAdapter extends BaseAdapter {
      *
      * @return Count of items.
      */
-    @Override
     public int getCount() {
         return itemList.getCount();
     }
@@ -48,9 +63,76 @@ public class ItemListAdapter extends BaseAdapter {
      *                 data set.
      * @return The data at the specified position.
      */
-    @Override
-    public Object getItem(int position) {
+    public Item getItem(int position) {
         return itemList.getItem(position);
+    }
+
+    /**
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using
+     * {@link #onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see #getItemViewType(int)
+     * @see #onBindViewHolder(ViewHolder, int)
+     */
+    @NonNull
+    @Override
+    public ItemListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        /* Need to inflate the custom layout */
+        View itemView = inflater.inflate(R.layout.view_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        return viewHolder;
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
+     * position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use {@link ViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Override {@link #onBindViewHolder(ViewHolder, int, List)} instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ItemListAdapter.ViewHolder holder, int position) {
+        /* Caching the item that will be used to fill up the row */
+        Item item = itemList.getItem(position);
+
+        /* Assigning the content of the view holder */
+        TextView nameView = holder.name;
+        TextView makeView = holder.make;
+        TextView idView = holder.id;
+
+        /* Setting the text of a row */
+        nameView.setText(item.getName());
+        makeView.setText(item.getMake());
+        idView.setText(item.getSerialNumber());
     }
 
     /**
@@ -65,32 +147,13 @@ public class ItemListAdapter extends BaseAdapter {
     }
 
     /**
-     * Get a View that displays the data at the specified position in the data set. You can either
-     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-     * parent View (GridView, ListView...) will apply default layout parameters unless you use
-     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
-     * to specify a root view and to prevent attachment to the root.
+     * Returns the total number of items in the data set held by the adapter.
      *
-     * @param position    The position of the item within the adapter's data set of the item whose view
-     *                    we want.
-     * @param convertView The old view to reuse, if possible. Note: You should check that this view
-     *                    is non-null and of an appropriate type before using. If it is not possible to convert
-     *                    this view to display the correct data, this method can create a new view.
-     *                    Heterogeneous lists can specify their number of view types, so that this View is
-     *                    always of the right type (see {@link #getViewTypeCount()} and
-     *                    {@link #getItemViewType(int)}).
-     * @param parent      The parent that this view will eventually be attached to
-     * @return A View corresponding to the data at the specified position.
+     * @return The total number of items in this adapter.
      */
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        /* Need to display to recycler view. */
-        return null;
-    }
-
-    /* Getters and Setters */
-    public void setContext(Context context) {
-        applicationContext = context;
+    public int getItemCount() {
+        return itemList.getCount();
     }
 
     public void setItemList(ItemList itemList) {
@@ -102,4 +165,5 @@ public class ItemListAdapter extends BaseAdapter {
     public void removeItem(int position) {
         this.itemList.remove(position);
     }
+
 }
--- code/app/src/main/java/com/example/sigma_blue/ViewListActivity.java
+++ code/app/src/main/java/com/example/sigma_blue/ViewListActivity.java
@@ -3,7 +3,12 @@ package com.example.sigma_blue;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Date;
 
 public class ViewListActivity extends BaseActivity{
     /* Tracking views that gets reused */
@@ -20,10 +25,25 @@ public class ViewListActivity extends BaseActivity{
         setContentView(R.layout.view_list);
 
         /* Setting up the data. TODO: Make this use the database */
-        this.itemListAdapter = ItemListAdapter.newInstance(ItemList.newInstance(), this);
+        this.itemListAdapter = ItemListAdapter.newInstance(ItemList.newInstance());
 
         /* Code section for linking UI elements */
         addEntryButton = findViewById(R.id.addButton);
         searchButton = findViewById(R.id.searchButton);
+        RecyclerView rvItemListView = findViewById(R.id.listView);
+
+        /* Adding to the adapter for testing */
+        itemListAdapter.addItem(new Item(
+                "3080",
+                new Date(),
+                "Description",
+                "EVGA",
+                "RTX 3080",
+                38.0f
+        ));
+
+        /* Linking the adapter to the UI */
+        rvItemListView.setAdapter(itemListAdapter);
+        rvItemListView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
