a8a4de8429e48887461ee22d2824fba9ce86125a
Bach <waridh@ualberta.ca>
2023-10-29 16:16:30 -0600

    Made the adaptable interface a generic

--- code/app/src/main/java/com/example/sigma_blue/IAdaptable.java
+++ code/app/src/main/java/com/example/sigma_blue/IAdaptable.java
@@ -4,8 +4,8 @@ package com.example.sigma_blue;
  * Interface that enforces methods that are required for a container to be
  * adaptable to the android BaseAdapter
  */
-public interface IAdaptable {
+public interface IAdaptable<E> {
     public int getCount();
-    public Object getItem(int position);
+    public E getItem(int position);
     public int getItemId(int position);
 }
