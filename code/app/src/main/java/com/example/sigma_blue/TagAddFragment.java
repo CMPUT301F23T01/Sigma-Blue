1ba55e28213add5d2a74675126a46af39d5c6b54
echeng-git <eccheng@ualberta.ca>
2023-10-29 21:09:32 -0600

    started a bit on addtaglayout

---
+++ code/app/src/main/java/com/example/sigma_blue/TagAddFragment.java
@@ -0,0 +1,19 @@
+package com.example.sigma_blue;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class TagAddFragment extends Fragment {
+    public TagAddFragment() {
+        super(R.layout.add_tag_fragment);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+    }
+
+}
