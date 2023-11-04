e847d9b0aab1927aa7d12d936a8e444ce4165193
bpaul2 <paulson2@ualberta.ca>
2023-11-04 13:01:33 -0600

    Progress on add/edit/details fragments

---
+++ code/app/src/main/java/com/example/sigma_blue/AddEditActivity.java
@@ -0,0 +1,17 @@
+package com.example.sigma_blue;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddEditActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_edit_activity);
+
+
+    }
+}
