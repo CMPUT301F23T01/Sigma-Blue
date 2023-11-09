abd5755c3c5bf4162f9afa6154815892c18dac61
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2023-11-09 09:37:08 -0700

    Added documentation to AccountDB.java

--- code/app/src/main/java/com/example/sigma_blue/AccountDB.java
+++ code/app/src/main/java/com/example/sigma_blue/AccountDB.java
@@ -8,6 +8,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * This class handles databse handling for accounts
+ */
 public class AccountDB extends ADatabaseHandler<Account> {
 
     private CollectionReference accountPointer;
@@ -20,19 +23,35 @@ public class AccountDB extends ADatabaseHandler<Account> {
         return ret;
     };
 
+    /**
+     * newInstance method for hiding construction.
+     * @return a new AccountDB instance.
+     */
     public static AccountDB newInstance() {
         return new AccountDB();
     }
 
+    /**
+     * Constructor for AccountDB
+     */
     private AccountDB() {
         accountPointer = FirebaseFirestore.getInstance()
                 .collection(DatabaseNames.PRIMARY_COLLECTION.getName());
     }
+
+    /**
+     * Method for adding a new account to the database
+     * @param item is an Account object that is being added
+     */
     @Override
     public void add(Account item) {
         addDocument(accountPointer, item, hashMapConverter, item.getDocID());
     }
 
+    /**
+     * Method for removing an existing account from the database
+     * @param item is the Account object to be removed
+     */
     @Override
     public void remove(final Account item) {
         removeDocument(accountPointer, item);
