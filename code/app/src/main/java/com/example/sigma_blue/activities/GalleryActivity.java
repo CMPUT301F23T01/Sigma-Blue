fb4289b68f06d86f587488ea1aec7c53416db9f6
Yunyang <yunyang5@ualberta.ca>
2023-11-25 22:00:23 -0700

    Intent trial, with source selection

--- code/app/src/main/java/com/example/sigma_blue/activities/GalleryActivity.java
+++ code/app/src/main/java/com/example/sigma_blue/activities/GalleryActivity.java
@@ -2,8 +2,13 @@ package com.example.sigma_blue.activities;
 
 import static com.example.sigma_blue.activities.ActivityLauncher.registerForActivityResult;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Gallery;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
@@ -42,6 +47,42 @@ public class GalleryActivity extends BaseActivity {
     }
 
      */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        chooseImage(GalleryActivity.this);
+    }
+
+
+    // function to let's the user to choose image from camera or gallery
+    // TODO: SHOULD NOT BE HERE, for testing purpose, should be refactored to somewhere else
+    private void chooseImage(Context context){
+        final CharSequence[] optionsMenu = {"Take Photo", "Choose from Gallery", "Exit" }; // create a menuOption Array
+        // create a dialog for showing the optionsMenu
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        // set the items in builder
+        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(optionsMenu[i].equals("Take Photo")){
+                    // Open the camera and get the photo
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                }
+                else if(optionsMenu[i].equals("Choose from Gallery")){
+                    // choose from  external storage
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+                }
+                else if (optionsMenu[i].equals("Exit")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
 
 }
+
+
 
