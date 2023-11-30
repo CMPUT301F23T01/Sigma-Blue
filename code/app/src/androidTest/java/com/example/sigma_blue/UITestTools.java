1addd2a13f7931725cce2174cb1bde8bee1365a0
Yunyang <yunyang5@ualberta.ca>
2023-11-29 17:02:18 -0700

    added some UI tools

--- code/app/src/androidTest/java/com/example/sigma_blue/UITestTools.java
+++ code/app/src/androidTest/java/com/example/sigma_blue/UITestTools.java
@@ -1,13 +1,46 @@
 package com.example.sigma_blue;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 
 public class UITestTools {
     public void closeKeyboard() {
         onView(ViewMatchers.isRoot()).perform(closeSoftKeyboard());
+    }
+
+    public void signup(String username, String password) {
+        onView(withId(R.id.createAccButton)).perform(click());
+        onView(withId(R.id.usernameEditText)).perform(ViewActions
+                .typeText(username));
+        onView(ViewMatchers.isRoot()).perform(closeSoftKeyboard());
+        onView(withId(R.id.passwordEditText)).perform(ViewActions
+                .typeText(password));
+        onView(ViewMatchers.isRoot()).perform(closeSoftKeyboard());
+        onView(withText("CONFIRM"))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
+    }
+    public void login(String username, String password) {
+        // login with new account
+        onView(withId(R.id.loginButton)).perform(click());
+        onView(withId(R.id.usernameEditText)).perform(ViewActions.typeText(username));
+        onView(ViewMatchers.isRoot()).perform(closeSoftKeyboard());
+        onView(withId(R.id.passwordEditText)).perform(ViewActions.typeText(password));
+        onView(ViewMatchers.isRoot()).perform(closeSoftKeyboard());
+        onView(withText("LOGIN"))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
     }
 }
