f6d23ce88b6c29b97b860f3ace142a02278872ce
Bach <90401236+waridh@users.noreply.github.com>
2023-12-03 10:59:04 -0700

    started fixing account testing

--- code/app/src/test/java/com/example/sigma_blue/AccountTest.java
+++ code/app/src/test/java/com/example/sigma_blue/AccountTest.java
@@ -6,19 +6,69 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import com.example.sigma_blue.entity.account.Account;
+import com.example.sigma_blue.utility.StringHasher;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class AccountTest {
     @Mock
     QueryDocumentSnapshot mockDocument;
+
+    private List<Account> cUTs;
+
+    @Before
+    public void makeCUTs() {
+        cUTs = new ArrayList<>();
+
+        cUTs.add(new Account("testing", "password1"));
+        cUTs.add(new Account("testing2", "Password2"));
+        cUTs.add(new Account("Testing", "p"));
+    }
+
+    @After
+    public void tearDown() {
+        cUTs = null;
+    }
+
+    private HashMap<String, Object> hashMapFormatter(String username,
+                                                     String password) {
+        String hashedPassword = StringHasher.getHash(password);
+        HashMap<String, Object> ret = new HashMap<>();
+        ret.put("USERNAME", username);
+        ret.put("PASSWORD", hashedPassword);
+        return ret;
+    }
+
+    @Test
+    public void testHashMapGeneration() {
+        // Testing creation of hashmap objects from the tags
+        HashMap<String, Object> expected1 = hashMapFormatter("testing",
+                "password1");
+        HashMap<String, Object> expected2 = hashMapFormatter("testing2",
+                "Password2");
+        HashMap<String, Object> expected3 = hashMapFormatter("Testing",
+                "p");
+
+        assertEquals(expected1, cUTs.get(0).getHashMapOfEntity().apply(cUTs
+                .get(0)));
+        assertEquals(expected2, cUTs.get(1).getHashMapOfEntity().apply(cUTs
+                .get(1)));
+        assertEquals(expected3, cUTs.get(2).getHashMapOfEntity().apply(cUTs
+                .get(2)));
+    }
 
     @Test
     public void testAccountCheckUsername(){
